d85b7b44828235be24ca42178ae4c68b94d78af7
Валодя Чёрный <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-01-07 04:33:55 +0300

    Add files via upload

---
+++ Trilogy Chaos Mod/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GTA SA Chaos Mod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GTA SA Chaos Mod")]
+[assembly: AssemblyCopyright("Copyright © 2019 Lordmau5")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("91106072-8642-4b48-aabe-f21a254fa788")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Trilogy Chaos Mod/GTAChaos_ProcessedByFody.cs
@@ -0,0 +1,5 @@
+﻿internal class GTAChaos_ProcessedByFody
+{
+  internal const string FodyVersion = "6.6.1.0";
+  internal const string Costura = "5.7.0";
+}
